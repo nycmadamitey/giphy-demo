418459a81d55f89619e623ace000d701f25bce5b
Michael Adamitey <madamitey@gmail.com>
2020-02-03 02:31:01 -0500

    Updates

---
+++ precache-manifest.c51677abc69140f7aef0df47baba7271.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a9b761189e355e1eecc68f541d6cb8",
+    "url": "/giphy-demo/index.html"
+  },
+  {
+    "revision": "81278c331fa5723f9705",
+    "url": "/giphy-demo/static/css/main.97efa707.chunk.css"
+  },
+  {
+    "revision": "49edc0e095ac36c40b8b",
+    "url": "/giphy-demo/static/js/2.e2c834a0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/giphy-demo/static/js/2.e2c834a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81278c331fa5723f9705",
+    "url": "/giphy-demo/static/js/main.f52db0ae.chunk.js"
+  },
+  {
+    "revision": "f3b06f76932c6e0264cc",
+    "url": "/giphy-demo/static/js/runtime-main.5e6a2b7a.js"
+  },
+  {
+    "revision": "e2db83e60eca5703b73db77f9072f5e3",
+    "url": "/giphy-demo/static/media/loader.e2db83e6.svg"
+  },
+  {
+    "revision": "62520ec392006e28c116e5e55901fd01",
+    "url": "/giphy-demo/static/media/search-icon.62520ec3.svg"
+  }
+]);
